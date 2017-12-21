d1077b3562ab9ce8734213664453381e02eeff64
Shtyrman <34683013+Shtyrman@users.noreply.github.com>
2017-12-21 14:17:07 +0300

    категории catId

--- routers/items.js
+++ routers/items.js
@@ -9,12 +9,12 @@ router.get('/items', (req, res) => {
     res.send(items);
 });
 
-router.get('/items/:id', function (req, res) {   
-   let tip = items.find(tip => tip.id == req.params.id);
+router.get('/items/:catId', function (req, res) {   
+   let tip = items.find(tip => tip.catId == req.params.catId);
 
-    res.send("категория:" + req.params["id"])
+    res.send("категория:" + req.params["catId"])
 });
-router.get('/items/:id/:itemId', function (req, res) {   
+router.get('/items/:catId/:itemId', function (req, res) {   
     let prod = items.find(prod => prod.itemId == req.params.itemId);
  
      res.send("продукт:" + req.params["itemId"])
