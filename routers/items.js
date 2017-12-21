a0cc3846868b7ddb33914d699d464ffe1bec1b55
Shtyrman <34683013+Shtyrman@users.noreply.github.com>
2017-12-21 14:12:55 +0300

    Изменения

--- routers/items.js
+++ routers/items.js
@@ -9,24 +9,15 @@ router.get('/items', (req, res) => {
     res.send(items);
 });
 
-
-// категории товаров
 router.get('/items/:id', function (req, res) {   
-    let tip = items.find(tip => tip.id == req.params.id);
+   let tip = items.find(tip => tip.id == req.params.id);
 
-    res.send("itemId:" + req.params["id"])
+    res.send("категория:" + req.params["id"])
 });
-// вот сами товары получаются в категории же, да?
-//
-// Пробовал вот такую конструкцию, типа категория/товар - но чет не получалось
-//
-//router.get('/items/:categoryId/:id', function (req, res) {   
-//
-//    let catId = item.find(catId => catId.id == req.params.id);
-//    let prodId = item.find(proId => prodId.product == req.params.product);
-//
-//   response.send(`Категория: ${catId}    Товар: ${prodId}`);
-//});
-// и в item.json [{ "id": categorija1, "product": "product1"}
+router.get('/items/:id/:itemId', function (req, res) {   
+    let prod = items.find(prod => prod.itemId == req.params.itemId);
+ 
+     res.send("продукт:" + req.params["itemId"])
+});
 
 module.exports = router;
