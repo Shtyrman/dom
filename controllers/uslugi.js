bf06f4530dfdb0ecc983ba4f049a34ef5ebc9753
Shtyrman <34683013+Shtyrman@users.noreply.github.com>
2018-01-03 16:55:30 +0300

    uslugi

--- controllers/uslugi.js
+++ controllers/uslugi.js
@@ -1,18 +1,32 @@
-const usluga = require('../data/uslugi');
+const uslugi = require('../data/uslugi');
 
 module.exports = {
+    findUsluga(req, res, next) {
+        let uslugaUrl = req.params.usluga;
+        let usluga = uslugi.find(usluga => usluga.url === uslugaUrl);
+
+        if (!usluga) {
+            let error = new Error('Книга не найдена');
+            error.status = 404;
+            next(error);
+        } else {
+            req.usluga = usluga;
+
+            next();
+        }
+    },
     // GET /uslugi
     showAllUslugi(req, res) {
         res.render('sayt/page/main', {
             id: 'uslugi',
-            title: 'CodeLibrary'
+            title: 'Мои услуги'
         });
     },
     // GET /uslugi/:usluga
     showUslugi(req, res) {
         res.render('sayt/page/main', {
             id: 'usluga',
-            title: req.params.usluga
+            title: req.usluga.usluga
         });
     }
 };
--- routers/uslugi.js
+++ routers/uslugi.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
 const { uslugi: {
+    findUsluga,
     showAllUslugi,
     showUslugi
 } } = require('../controllers');
 
 
 router.get('/', showAllUslugi);
-router.get('/:usluga', showUslugi);
+router.get('/:usluga', findUsluga, showUslugi);
 
 module.exports = router;
